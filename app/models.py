78e09be7167e2662fa7e1711cbafe92fb9fd5205
Moringa School <wangu.mwangi@student.moringaschool.com>
2022-05-07 18:42:49 +0300

    models:import datetime

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from . import db, login_manager
+from datetime import datetime
 from flask_login import UserMixin ,current_user
 from  werkzeug.security import generate_password_hash,check_password_hash
 
