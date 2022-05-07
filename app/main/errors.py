ac21e3263d8216e2be02a68fb0f3cd1c0357ea00
Moringa School <wangu.mwangi@student.moringaschool.com>
2022-05-07 21:10:25 +0300

    main/error:created function to render an error message incase of page not found

--- app/main/errors.py
+++ app/main/errors.py
@@ -1,3 +1,8 @@
-from flask import render_template,redirect,url_for,abort,request
+from flask import render_template
+from . import main
 
+@main.errorhandler(404)
+def four_oh_four(error):
+    
+    return render_template('404.html'),404
    
