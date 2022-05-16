eb0fbd07f26152ce777c5adbd261c198c71da69d
Elijah Mwangi Wangu <97939786+elijah-dot@users.noreply.github.com>
2022-05-16 23:54:54 +0300

    Delete start.sh

--- start.sh
+++
@@ -1,8 +0,0 @@
-export SECRET_KEY = '$V5F;;:8&Y7hdmnTve}Vx;)NKxmgpC'
-
-
-
-export MAIL_USERNAME='wangu.mwangi@student.moringaschool.com'
-export MAIL_PASSWORD='Elijah2000!'
-
-python3.8 manage.py server
